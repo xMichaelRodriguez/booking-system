faec40332b5aafa106bd603ceb61317220875f8d
xMichaelRodriguez <scottlovos503@gmail.com>
2023-03-16 20:56:10 -0600

    refactor: cloudinary provider

--- src/modules/cloudinary/Cloudinary.module.ts
+++ src/modules/cloudinary/Cloudinary.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { CloudinaryProvider } from './cloudinary.provider';
+import { Cloudinary } from './cloudinary';
 import { CloudinaryService } from './cloudinary.service';
 
 @Module({
-  providers: [CloudinaryProvider, CloudinaryService],
-  exports: [CloudinaryProvider, CloudinaryService],
+  providers: [CloudinaryService, Cloudinary],
+  exports: [CloudinaryService],
 })
 export class CloudinaryModule {}
