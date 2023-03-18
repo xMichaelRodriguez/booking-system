d08762137cd2ccfcc944a42169cea72215805e4c
xMichaelRodriguez <scottlovos503@gmail.com>
2023-03-17 19:26:18 -0600

    refactor: absolute paths

--- src/modules/booking-services/services.module.ts
+++ src/modules/booking-services/services.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ConfigurationService } from '@/config/configuration';
 import { CloudinaryModule } from '@/modules/media/Cloudinary.module';
-import { CloudinaryService } from '@/modules/media/cloudinary.service';
 
+import { CloudinaryService } from '../media/Cloudinary.service';
 import { Services } from './entities/services.entity';
 import { BookingServicesController } from './services.controller';
 import { BookingServicesService } from './services.service';
